26dc6125463c15d9af1ddef2ff011780dcd5970a
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-10-30 12:03:18 -0300

    Resolving import

--- front-end/crud-users-products/src/App.js
+++ front-end/crud-users-products/src/App.js
@@ -1,6 +1,6 @@
 import GlobalStyle from "./styles/global";
-import Users from "./pages/Users.js";
-import Orders from "./pages/Orders.js";
+import Users from "./pages/Users/Users.js";
+import Orders from "./pages/Orders/Orders.js";
 import Home from "./pages/Home.js";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
